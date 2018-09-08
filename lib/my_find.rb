d4f0949611ccdea89cc8288eb8447087e83da9e9
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 16:41:05 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,9 @@ def my_find(collection)
   if collection != []
     i = 0 
     while i < collection.length 
-    return collection[i] if yield(collection[i])
-    i = i + 1   
+      return collection[i] if yield(collection[i])
+      i = i + 1
+    end
   else
     "No data in collection"
   end
